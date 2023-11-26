7272265a33f70b96a3682038c6b7b86516906dce
Sherxon <116145014+SherxonBoboyorov@users.noreply.github.com>
2023-11-26 11:46:46 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+
+// $favcolor = "red";
+
+// switch($favcolor) {
+//     case "red":
+//         echo "Your favorite color is red!";
+//         break;
+    
+//     case "blue":
+//         echo "Your favorite color is blue";
+//         break;
+    
+//     case "green":
+//         echo "Your favorite color is green";
+//         break;
+//     default:
+//         echo "Your favorite color is neither red, blue, green!";
+// }
+
+
+// switch bu hisoblash;
+
+$x = 52;
+$y = 5;
+
+/* 
+    and - va,
+    or - yoki,
+    not - teskari / yo'q
+*/
+
+if ($x == $y) {
+    echo 'x teng y';
+}else {
+    echo 'teng emas';
+}
+
+
+?>
+
+
+
