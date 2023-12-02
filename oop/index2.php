dd7b92957ddc50ea3ed32c9f9fb03f441361db0a
Boboyorov <sherxonboboyorov@gmail.com>
2023-12-02 17:03:34 +0500

    some changes

--- oop/index2.php
+++ oop/index2.php
@@ -1,37 +1,55 @@
 <?php
 
-class Car {
-    public $model;
-    public $year;
+// Inheritance
+// extends
+// parent - child
+// overiding
+// final
 
-    public function __construct($model, $year)
-    {
-        $this->model = $model;
-        $this->year = $year;
-    }
+// class Car {
+//     public $model;
+//     public $year;
 
-    public function drive() 
-    {
-        return 'driving';
-    }
+//     public function __construct($model, $year)
+//     {
+//         $this->model = $model;
+//         $this->year = $year;
+//     }
 
-    public function getFuel()
-    {
-        return 'fueling';
-    }
+//     public function drive() 
+//     {
+//         return 'driving';
+//     }
 
-    public function setModal($model)
-    {
-        $this->model = $model;
-    }
+//     public function getFuel()
+//     {
+//         return 'fueling';
+//     }
 
-}
+//     public function setModal($model)
+//     {
+//         $this->model = $model;
+//     }
+// }
 
 
-$bmw = new Car('Bmw', '2019');
+// $bmw = new Car('Bmw', '2019');
 
-$audi = new Car('Audi', 2023);
+// $audi = new Car('Audi', 2023);
 
-$chevy = new Car('Chevrolet', 2018);
+// $chevy = new Car('Chevrolet', 2018);
 
-var_dump($chevy->year);
+// var_dump($chevy->year);
+
+
+
+
+
+
+
+
+
+
+
+
+
