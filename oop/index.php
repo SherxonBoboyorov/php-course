b8fc86cefa24fbe490e69a1404959e766b5d3cfb
Boboyorov <sherxonboboyorov@gmail.com>
2023-12-02 14:44:37 +0500

    some changes

--- oop/index.php
+++ oop/index.php
@@ -16,23 +16,7 @@ function __destruct() {
 
 $apple = new Fruit("Apple", "red");
 
-
  
-
-
-// class Fruit {
-//     public $name;
-//     public $color;
-  
-//     function __construct($name) {
-//       $this->name = $name;
-//     }
-//     function __destruct() {
-//       echo "The fruit is {$this->name}.";
-//     }
-//   }
-  
-//   $apple = new Fruit("Apple");
   
 
 
--- oop/index2.php
+++ oop/index2.php
@@ -1,32 +1,37 @@
 <?php
 
-// class Car {
-//     public $model;
-//     public $year;
+class Car {
+    public $model;
+    public $year;
 
-//     public function __construct($model, $year)
-//     {
-//         $this->model = $model;
-//         $this->year = $year;
-//     }
+    public function __construct($model, $year)
+    {
+        $this->model = $model;
+        $this->year = $year;
+    }
 
-//     public function drive() 
-//     {
-//         return 'driving';
-//     }
+    public function drive() 
+    {
+        return 'driving';
+    }
 
-//     public function getFuel()
-//     {
-//         return 'fueling';
-//     }
+    public function getFuel()
+    {
+        return 'fueling';
+    }
 
-// }
+    public function setModal($model)
+    {
+        $this->model = $model;
+    }
+
+}
 
 
-// $bmw = new Car('Bmw', '2019');
+$bmw = new Car('Bmw', '2019');
 
-// $audi = new Car('Audi', 2023);
+$audi = new Car('Audi', 2023);
 
-// $chevy = new Car('Chevrolet', 2018);
+$chevy = new Car('Chevrolet', 2018);
 
-// var_dump($chevy->year);
+var_dump($chevy->year);
