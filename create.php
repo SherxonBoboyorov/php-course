8d9bf49e537af737375301e5136b40c6d3308280
Boboyorov <sherxonboboyorov@gmail.com>
2023-12-07 16:12:31 +0500

    form end

--- create.php
+++ create.php
@@ -1,5 +1,42 @@
 <?php 
 
-echo 'salom world';
+require_once 'config/bootstrap.php';
+
+if($_SERVER['REQUEST_METHOD'] == 'POST' ){
+    $title = $_POST['title'];
+    $body = $_POST['body'];
+
+   $result =  Post::create($title, $body);
+
+   var_dump($result);
+}
+
 
 ?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create Post</title>
+</head>
+<body>
+
+    <form method="POst" action="">
+        <h4>Create Post</h4>
+        <div>
+            <input type="text" name="title" placeholder="title">
+        </div><br>
+        <div>
+            <textarea name="body" cols="30" rows="10" placeholder="body"></textarea>
+        </div><br>
+        <div>
+            <button type="submit">Submit</button>
+        </div>
+    </form>
+        
+</body>
+</html>
