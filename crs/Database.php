b36af8292f44704c9c067b412d0c7257981227f6
Boboyorov <sherxonboboyorov@gmail.com>
2024-01-22 15:33:12 +0500

    bazi bir uzgarishlar

--- crs/Database.php
+++ crs/Database.php
@@ -1,19 +1,18 @@
 <?php
 
-
-class Database 
+class Database
 {
-       public $servername;
-       public $database;
-       public $username;
-       public $password;
+
+    public $servername;
+    public $database;
+    public $username;
+    public $password;
 
     public function __construct($servername, $database, $username, $password)
     {
         $this->servername = $servername;
         $this->database = $database;
         $this->username = $username;
-        $this->password = $password;
         $this->password = $password;
 
         $this->connect();
@@ -22,24 +21,14 @@ class Database
     public function connect()
     {
         try {
-            $conn = new PDO("mysql:host=$this->servername;dbname=$this->database", $this->username, $this->password);
+            $conn = new PDO("mysql:host+$this->servername;dbname=$this->database", $this->username, $this->password);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                return $conn;
-            }
-                catch(PDOException $e)
-            {
+            return $conn;
+        } catch (PDOException $e) {
             echo "Connection failed: " . $e->getMessage();
 
-            echo "19.01.2024"; // bu shunchaki githubda branch ni tekshirish uchun qilindi
-            
         }
     }
-
-
-
-    
 }
 
-
 ?>
-
