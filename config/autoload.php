c143f45f51e135d831c8a403abdb5118f9b3ec80
Sherxon <116145014+SherxonBoboyorov@users.noreply.github.com>
2024-12-15 19:44:15 +0000

    some changes

--- config/autoload.php
+++ config/autoload.php
@@ -4,4 +4,10 @@ spl_autoload_register(function ($class_name) {
     include 'crs/' . $class_name . '.php';
 })
 
+spl_autoload_register(function ($argc)) {
+    include 'crs/' . $class_name . '.php';
+}
+
+return true;
+
 ?>
